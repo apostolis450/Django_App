202eaf8d36d803012ce1f2aef749f167aeb58c25
Apostolis <apostolis450@gmail.com>
2021-02-13 22:26:25 +0200

    login/logout functionality

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -5,25 +5,51 @@ from .models import *
 from .forms import OrderForm,CreateUserForm
 from .filters import OrderFilter
 from django.contrib.auth.forms import UserCreationForm
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages 
+from django.contrib.auth.decorators import login_required
 
+# Create your views here
 def registerPage(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user )
+                return redirect('login')
 
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-
-    context = {'form': form}
-    return render(request, 'accounts/register.html', context)
+        context = {'form': form}
+        return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
-    context = {}
-    return render(request, 'accounts/login.html', context)
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'username OR password is incorrect')
+                
+        context = {}
+        return render(request, 'accounts/login.html', context)
 
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
-
+@login_required(login_url='login')
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
